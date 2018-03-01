e39b2c74b02a245060c0c9c50a0b604e7d7fbb87
Jiarong Gu <jiaronggu@hotmail.com>
2018-03-02 08:27:17 +1100

    update model

--- src/BanBrick.Infrastructure.Repositories/Models/CssComponent.cs
+++ src/BanBrick.Infrastructure.Repositories/Models/CssComponent.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
 
@@ -14,7 +15,7 @@ namespace BanBrick.Infrastructure.Repositories.Models
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public string Id { get; set; }
 
-        public string Identifier { get; set; }
+        public string QuerySelector { get; set; }
 
 
     }
